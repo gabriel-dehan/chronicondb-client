d3a02ba2216ac5b5a5392f36477b091fb0edcaff
Gabriel Dehan <dehan.gabriel@gmail.com>
2020-11-15 23:42:59 +0100

    Fonts and variables

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,9 @@ import ReactDOM from 'react-dom';
 
 import reportWebVitals from './reportWebVitals';
 
-// import 'src/assets/styles/global.css';
+import 'src/assets/styles/fonts.css';
+import 'src/assets/styles/variables.css';
+import 'src/assets/styles/global.css';
 
 // Mobx : all state that is observed somewhere needs to be changed through actions
 configure({ enforceActions: 'observed' });
