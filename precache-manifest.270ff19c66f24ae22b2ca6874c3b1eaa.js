0be3678672d29b796ace157244040c6cfd99755a
Gabriel Dehan <dehan.gabriel@gmail.com>
2020-12-09 21:28:52 +0100

    Updates

---
+++ precache-manifest.270ff19c66f24ae22b2ca6874c3b1eaa.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27359cd742ea925a44431c6e17d4785",
+    "url": "/index.html"
+  },
+  {
+    "revision": "467649605fa75b85f2e7",
+    "url": "/static/css/2.623391d9.chunk.css"
+  },
+  {
+    "revision": "5a1d501099e70d735a30",
+    "url": "/static/css/main.0060f008.chunk.css"
+  },
+  {
+    "revision": "467649605fa75b85f2e7",
+    "url": "/static/js/2.883b04b7.chunk.js"
+  },
+  {
+    "revision": "243eabe987ac63b54849d76b548daaaa",
+    "url": "/static/js/2.883b04b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef368748cb7dbe27cef",
+    "url": "/static/js/3.31cb8a67.chunk.js"
+  },
+  {
+    "revision": "1688c09c165371e41d07",
+    "url": "/static/js/4.6eb9b74d.chunk.js"
+  },
+  {
+    "revision": "765fc14204b5e1e97a30",
+    "url": "/static/js/5.c0585973.chunk.js"
+  },
+  {
+    "revision": "5a1d501099e70d735a30",
+    "url": "/static/js/main.b553b687.chunk.js"
+  },
+  {
+    "revision": "aae536c87654ca537a5f",
+    "url": "/static/js/runtime-main.c0deb137.js"
+  },
+  {
+    "revision": "94ef9ba9b4c6f470223d9d06457c8228",
+    "url": "/static/media/Kufam-Bold.94ef9ba9.woff2"
+  },
+  {
+    "revision": "a93292fa3af1d07341651480d28b10bf",
+    "url": "/static/media/Kufam-Bold.a93292fa.woff"
+  },
+  {
+    "revision": "42e44dee0b8b240135c3940dd34fd699",
+    "url": "/static/media/Kufam-BoldItalic.42e44dee.woff"
+  },
+  {
+    "revision": "9597a01f8e988da99b217a80ca481dee",
+    "url": "/static/media/Kufam-BoldItalic.9597a01f.woff2"
+  },
+  {
+    "revision": "08daceadac914f639c54687a69699a25",
+    "url": "/static/media/Kufam-Italic.08dacead.woff2"
+  },
+  {
+    "revision": "da260a877711026da1e512df2d1d5501",
+    "url": "/static/media/Kufam-Italic.da260a87.woff"
+  },
+  {
+    "revision": "63723fd4a6d2117c4eae47f4eb859698",
+    "url": "/static/media/Kufam-Medium.63723fd4.woff2"
+  },
+  {
+    "revision": "9ca1e4fc355429a7504fc689d8e367fc",
+    "url": "/static/media/Kufam-Medium.9ca1e4fc.woff"
+  },
+  {
+    "revision": "305f80f6a781518ef533e028834f1849",
+    "url": "/static/media/Kufam-MediumItalic.305f80f6.woff2"
+  },
+  {
+    "revision": "fb7b8068fc25ba943529e1fdfe033202",
+    "url": "/static/media/Kufam-MediumItalic.fb7b8068.woff"
+  },
+  {
+    "revision": "2586e0e049dc50cfa2e8bf90c4177b9b",
+    "url": "/static/media/Kufam-Regular.2586e0e0.woff2"
+  },
+  {
+    "revision": "9682699441ac018519be1b83f74cb01c",
+    "url": "/static/media/Kufam-Regular.96826994.woff"
+  },
+  {
+    "revision": "05781ea4b13eafa25a3438b1532feaf7",
+    "url": "/static/media/NunitoSans-Bold.05781ea4.woff2"
+  },
+  {
+    "revision": "ebf9bf9d83de15d48ea891917271dc48",
+    "url": "/static/media/NunitoSans-Bold.ebf9bf9d.woff"
+  },
+  {
+    "revision": "1398c8a28ad762a95edace3d01a83f33",
+    "url": "/static/media/NunitoSans-BoldItalic.1398c8a2.woff2"
+  },
+  {
+    "revision": "7944f33de8865ad27b054fc4b5717f63",
+    "url": "/static/media/NunitoSans-BoldItalic.7944f33d.woff"
+  },
+  {
+    "revision": "6ea743e1b4d6da264dcb2ad97eb26f11",
+    "url": "/static/media/NunitoSans-Italic.6ea743e1.woff2"
+  },
+  {
+    "revision": "aaef6fa0b012770d1e65a578c8542ec4",
+    "url": "/static/media/NunitoSans-Italic.aaef6fa0.woff"
+  },
+  {
+    "revision": "514e30d9cc65400fed47c8af4489a9a1",
+    "url": "/static/media/NunitoSans-Regular.514e30d9.woff"
+  },
+  {
+    "revision": "990fe215c5627c98e8591be452a30c12",
+    "url": "/static/media/NunitoSans-Regular.990fe215.woff2"
+  },
+  {
+    "revision": "0e54fd067ba45c92130bf0718cae938a",
+    "url": "/static/media/NunitoSans-SemiBold.0e54fd06.woff2"
+  },
+  {
+    "revision": "e148d790df8c97c6f1d4abfcf3e7e271",
+    "url": "/static/media/NunitoSans-SemiBold.e148d790.woff"
+  },
+  {
+    "revision": "7c9344349cf14487c6d6f4890868232f",
+    "url": "/static/media/NunitoSans-SemiBoldItalic.7c934434.woff2"
+  },
+  {
+    "revision": "805b8010d8376af02dc01ab98100bae4",
+    "url": "/static/media/NunitoSans-SemiBoldItalic.805b8010.woff"
+  },
+  {
+    "revision": "827bf1913aa50e484eb2834c6c68c0f1",
+    "url": "/static/media/Philosopher-Bold.827bf191.woff2"
+  },
+  {
+    "revision": "c1f8c134cc34d6a02faafc2123b449de",
+    "url": "/static/media/Philosopher-Bold.c1f8c134.woff"
+  },
+  {
+    "revision": "4ed957f07760278d5d65b7aa476786e5",
+    "url": "/static/media/Philosopher-BoldItalic.4ed957f0.woff2"
+  },
+  {
+    "revision": "7001fe9419de5f6c2f99de4ab7e4156e",
+    "url": "/static/media/Philosopher-BoldItalic.7001fe94.woff"
+  },
+  {
+    "revision": "00314fa7028482ddaab5c31dad744bf2",
+    "url": "/static/media/Philosopher-Italic.00314fa7.woff2"
+  },
+  {
+    "revision": "36598abfcbe4b995a905e3ca70bf1edf",
+    "url": "/static/media/Philosopher-Italic.36598abf.woff"
+  },
+  {
+    "revision": "31d0614690e29526b39a0e7f4d272821",
+    "url": "/static/media/Philosopher-Regular.31d06146.woff2"
+  },
+  {
+    "revision": "4dc960867fa0324fd2b3c9143d0fba42",
+    "url": "/static/media/Philosopher-Regular.4dc96086.woff"
+  },
+  {
+    "revision": "8ebe65e4a35966ba455f00b1c0330d85",
+    "url": "/static/media/VPPixel-Simplified.8ebe65e4.woff"
+  },
+  {
+    "revision": "fe07a7c7a539d46962b6ccb9f59e9b90",
+    "url": "/static/media/VPPixel-Simplified.fe07a7c7.woff2"
+  },
+  {
+    "revision": "c9c5609caaba6d2ce79b1612877f17a1",
+    "url": "/static/media/body.c9c5609c.png"
+  },
+  {
+    "revision": "495548ef986b87b1b830ff04cfcc714a",
+    "url": "/static/media/body_background.495548ef.png"
+  },
+  {
+    "revision": "7ac7186b29719c1748623c13d07ff0e3",
+    "url": "/static/media/content_background.7ac7186b.png"
+  },
+  {
+    "revision": "0674676802bae3522a896f8157803a0b",
+    "url": "/static/media/sidebar.06746768.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chronicondb-client/precache-manifest.02144c68c9e592d94750264b38aa9d12.js"
+  "/precache-manifest.270ff19c66f24ae22b2ca6874c3b1eaa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chronicondb-client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
