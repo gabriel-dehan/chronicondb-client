6b71e3b3095c1210c87b074924f1e9bbeaa3aab0
Gabriel Dehan <dehan.gabriel@gmail.com>
2020-12-17 14:48:54 +0100

    1.10.8 Update

---
+++ src/engine/data/1.10.8/extracts/index.ts
@@ -0,0 +1,20 @@
+
+import enchants from './enchants.json';
+import enchantsSearchIndex from './enchantsSearchIndex.json';
+import items from './items.json';
+import itemsSearchIndex from './itemsSearchIndex.json';
+import sets from './sets.json';
+import skills from './skills.json';
+import skillsByClass from './skillsByClass.json';
+import skillsSearchIndex from './skillsSearchIndex.json';
+
+export default {
+  items,
+  enchants,
+  sets,
+  skills,
+  skillsByClass,
+  itemsSearchIndex,
+  enchantsSearchIndex,
+  skillsSearchIndex,
+};
--- src/engine/data/index.ts
+++ src/engine/data/index.ts
@@ -3,10 +3,12 @@ import V1102 from './1.10.2/extracts';
 import V1104 from './1.10.4/extracts';
 import V1106 from './1.10.6/extracts';
 import V1107 from './1.10.7/extracts';
+import V1108 from './1.10.8/extracts';
 
 export default {
   '1.10.2': V1102,
   '1.10.4': V1104,
   '1.10.6': V1106,
   '1.10.7': V1107,
+  '1.10.8': V1108,
 };
