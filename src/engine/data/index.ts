7faab93824af9d6e28a3b0c3a29ebfd2f96a29f1
iconmaster5326 <crazybonesj@yahoo.com>
2021-12-15 10:54:55 -0500

    Add new data for 1.31.1

--- src/engine/data/index.ts
+++ src/engine/data/index.ts
@@ -7,6 +7,7 @@ import V1108 from './1.10.8/extracts';
 import V1113 from './1.11.3/extracts';
 import V1202 from './1.20.2/extracts';
 import V1300 from './1.30.0/extracts';
+import V1311 from './1.31.1/extracts';
 
 export default {
   '1.10.2': V1102,
@@ -17,4 +18,5 @@ export default {
   '1.11.3': V1113,
   '1.20.2': V1202,
   '1.30.0': V1300,
+  '1.31.1': V1311,
 };
